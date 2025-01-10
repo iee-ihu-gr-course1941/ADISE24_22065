e9c0a7014e54c46848be5a4d1d5ab452e9d97b02
Μαριάννα Βουράκη <mariannavourakii@gmail.com>
2025-01-10 14:46:01 +0200

    changes 10/01

--- server/routes/roomRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRoom, joinRoom } = require('../controllers/roomController');
-
-router.post('/createRoom', createRoom);
-router.post('/joinRoom', joinRoom);
-
-module.exports = router;
